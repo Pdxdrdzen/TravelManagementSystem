9e5351b348785a6cca51ca871de597927360a071
Przemysław Drdzeń <s094022@student.tu.kielce.pl>
2024-11-18 17:39:04 +0100

    Upload New File

---
+++ src/Travel_Management_System/Splash.java
@@ -0,0 +1,15 @@
+package Travel_Management_System;
+
+import javax.swing.*;
+public class Splash extends JFrame {
+
+    Splash() {
+        setSize(1200,600);
+        setLocation(300,200);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
